fa4f96cb895df78a05b73de79358d82aae0198fd
burak_msz <burakbilec123@gmail.com>
2026-09-24 16:53:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const OYUNLAR = [
 // KONFIGURASYON
 // ============================================================
 const gecerliKod = "ııOOOııııuuu?1éııOOOııııuuu?1é";
-let tikRengi = "purple";
+let tikRengi = "blue";
 const GUVENLIK_CEVABI = "msz-şifremi-unuttum";
 
 const BROKER_URL = 'wss://broker.emqx.io:8084/mqtt';
